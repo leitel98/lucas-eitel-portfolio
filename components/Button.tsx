57df755f8a50865a6f41be099c3e18028dc035fb
Lucas Eitel Cereceda <leitel1@uc.cl>
2023-11-29 17:05:27 -0300

    button

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,7 +15,7 @@ const Button: FC<ButtonI> = ({
   disabled,
   size,
 }) => {
-  const buttonClassName = `px-8 py-4 rounded-full shadow-sm transition-all duration-100 hover:scale-105 ${
+  const buttonClassName = `px-8 py-4 rounded-full shadow-sm transition-all duration-50 hover:scale-105 ${
     variant === 'gradient'
       ? 'bg-gradient-to-br from-royal-300 to-royal-500 text-royal-950 hover:bg-gradient-to-br hover:from-royal-200 hover:to-royal-400'
       : 'bg-black text-royal-300 border border-royal-300 hover:bg-royal-950'
